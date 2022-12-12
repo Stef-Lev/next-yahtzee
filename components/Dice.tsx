152c353f8a66198b29c67b66c7e15bb792fe6117
Stefanos Leventis <sleventis@ph.esl-asia.com>
2022-12-13 00:42:19 +0200

    changed dice styling, added selected die PoC

--- components/Dice.tsx
+++ components/Dice.tsx
@@ -1,38 +1,22 @@
 const Dice = ({ nameN, nameS }: { nameN: number; nameS: string }) => {
+  function handleClick(e) {
+    if (e.currentTarget.classList.contains("selected")) {
+      e.currentTarget.classList.remove("selected");
+    } else {
+      e.currentTarget.classList.add("selected");
+    }
+    console.log(e.currentTarget.classList);
+  }
+
   return (
-    <div id={`dice${nameN}`} className={`dice dice-${nameS}`}>
-      <div id={`dice-${nameS}-side-one`} className="side one">
-        <div className="dot one-1"></div>
-      </div>
-      <div id={`dice-${nameS}-side-two`} className="side two">
-        <div className="dot two-1"></div>
-        <div className="dot two-2"></div>
-      </div>
-      <div id={`dice-${nameS}-side-three`} className="side three">
-        <div className="dot three-1"></div>
-        <div className="dot three-2"></div>
-        <div className="dot three-3"></div>
-      </div>
-      <div id={`dice-${nameS}-side-four`} className="side four">
-        <div className="dot four-1"></div>
-        <div className="dot four-2"></div>
-        <div className="dot four-3"></div>
-        <div className="dot four-4"></div>
-      </div>
-      <div id={`dice-${nameS}-side-five`} className="side five">
-        <div className="dot five-1"></div>
-        <div className="dot five-2"></div>
-        <div className="dot five-3"></div>
-        <div className="dot five-4"></div>
-        <div className="dot five-5"></div>
-      </div>
-      <div id={`dice-${nameS}-side-six`} className="side six">
-        <div className="dot six-1"></div>
-        <div className="dot six-2"></div>
-        <div className="dot six-3"></div>
-        <div className="dot six-4"></div>
-        <div className="dot six-5"></div>
-        <div className="dot six-6"></div>
+    <div className="outline" onClick={handleClick}>
+      <div id={`dice${nameN}`} className={`dice dice-${nameS}`}>
+        <div id={`dice-${nameS}-side-one`} className="side one"></div>
+        <div id={`dice-${nameS}-side-two`} className="side two"></div>
+        <div id={`dice-${nameS}-side-three`} className="side three"></div>
+        <div id={`dice-${nameS}-side-four`} className="side four"></div>
+        <div id={`dice-${nameS}-side-five`} className="side five"></div>
+        <div id={`dice-${nameS}-side-six`} className="side six"></div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
-import Dice from "../components/dice";
+import React from "react";
+import Dice from "../components/Dice";
 
 function App() {
   const diceNames = [
-    { number: 1, string: "one" },
-    { number: 2, string: "two" },
-    { number: 3, string: "three" },
-    { number: 4, string: "four" },
-    { number: 5, string: "five" },
-    { number: 6, string: "six" },
+    { number: 1, string: "one", selected: false },
+    { number: 2, string: "two", selected: false },
+    { number: 3, string: "three", selected: false },
+    { number: 4, string: "four", selected: false },
+    { number: 5, string: "five", selected: false },
   ];
 
   function rollDice() {
     for (let k = 1; k <= diceNames.length; k++) {
       let dice = document.getElementById(`dice${k}`);
-      let diceRoll = Math.floor(Math.random() * 6 + 1);
+      let diceRoll = Math.floor(Math.random() * 5 + 1);
       console.log(`dice: ${k} - roll: ${diceRoll}`);
-      for (let i = 1; i <= 6; i++) {
-        dice.classList.remove("show-" + i);
+      for (let i = 1; i <= 5; i++) {
+        dice?.classList.remove("show-" + i);
         if (diceRoll === i) {
-          dice.classList.add("show-" + i);
+          dice?.classList.add("show-" + i);
         }
       }
     }
@@ -29,11 +29,11 @@ function App() {
       <div
         className="
       rounded-lg 
-      w-full md:w-3/4 xl:w-3/5
+      w-full lg:w-5/6 xl:w-3/5
       h-full md:h-[85%] 
       bg-basic-white"
       >
-        <div className="md:rounded-t-lg bg-gradient-to-r from-teal-blue to-teal-dark h-[40%] xs:p-[12px] md:p-[20px]">
+        <div className="md:rounded-t-lg bg-gradient-to-r from-teal-blue to-teal-dark xs:p-[12px] md:p-[20px] lg:p-[50px]">
           <h1>Yahtzee!</h1>
           <div className="flex justify-center items-center my-10">
             <div className="container flex justify-between">
@@ -45,10 +45,16 @@ function App() {
             </div>
           </div>
         </div>
-        <div id="roll" className="roll-button">
-          <button onClick={rollDice}>Roll dice!</button>
+        <div id="roll" className="roll-button mt-5">
+          <button
+            onClick={rollDice}
+            className="bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Roll dice!
+          </button>
         </div>
         <div className="p-[20px] h-[60%]"></div>
+        {/* <img src="/dice-sides/five.svg" /> */}
       </div>
     </div>
   );
